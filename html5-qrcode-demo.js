0744d1c17c1f553787ad3f12059b285671ad4c55
Emmanuel Engreso <enrgeso.emmanuel@gmail.com>
2022-11-24 23:47:48 +0800

    script changes

--- html5-qrcode-demo.js
+++ html5-qrcode-demo.js
@@ -1,19 +1,17 @@
-(function () {
-    function onScanSuccess(decodedText, decodedResult) {
-        // handle the scanned code as you like, for example:
-        console.log(`Code matched = ${decodedText}`, decodedResult);
-    }
+function onScanSuccess(decodedText, decodedResult) {
+    // handle the scanned code as you like, for example:
+    console.log(`Code matched = ${decodedText}`, decodedResult);
+}
 
-    function onScanFailure(error) {
-        // handle scan failure, usually better to ignore and keep scanning.
-        // for example:
-        console.warn(`Code scan error = ${error}`);
-    }
+function onScanFailure(error) {
+    // handle scan failure, usually better to ignore and keep scanning.
+    // for example:
+    console.warn(`Code scan error = ${error}`);
+}
 
-    let html5QrcodeScanner = new Html5QrcodeScanner(
-        'reader',
-        { fps: 10, qrbox: { width: 300, height: 300 } },
-        /* verbose= */ false
-    );
-    html5QrcodeScanner.render(onScanSuccess, onScanFailure);
-})();
+let html5QrcodeScanner = new Html5QrcodeScanner(
+    'reader',
+    { fps: 10, qrbox: { width: 300, height: 300 } },
+    /* verbose= */ false
+);
+html5QrcodeScanner.render(onScanSuccess, onScanFailure);
